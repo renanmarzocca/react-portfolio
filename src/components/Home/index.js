10ef3373b5a063d58d6defc14d044958e1aff94b
Renan Marzocca <renan.marzocca@gmail.com>
2023-05-01 17:31:37 -0300

    Atualização loader and page about

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import LogoTitle from '../../assets/images/logo-r-home.png'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from '../Logo'
 import './index.scss'
+import Loader from 'react-loaders'
 
 
 const Home = () => {
@@ -34,6 +35,7 @@ const Home = () => {
   }, [])
 
   return (
+    <>
       <div className="container home-page">
         <div className="text-zone">
           <h1>
@@ -65,6 +67,8 @@ const Home = () => {
         </div>
         <Logo />
       </div>
+      <Loader type="pacman" />
+      </>
   )
 }
 
